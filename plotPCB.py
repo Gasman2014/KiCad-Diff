d4d284581a986640b1f59991c3fc62be3caefbf3
Leandro <leoheck@gmail.com>
2020-09-22 20:57:23 -0300

    Revert renameing

--- plotPCB.py
+++ plotPCB.py
@@ -26,9 +26,7 @@ def processBoard(boardName, plotDir):  # Load board and initialize plot controll
     boardyl = boardbox.GetY()
     boardwidth = boardbox.GetWidth()
     boardheight = boardbox.GetHeight()
-
-    if not quiet:
-        print(boardxl, boardyl, boardwidth, boardheight)
+    print(boardxl, boardyl, boardwidth, boardheight)
 
     pctl = pcbnew.PLOT_CONTROLLER(board)
     pctl.SetColorMode(True)
@@ -43,31 +41,6 @@ def processBoard(boardName, plotDir):  # Load board and initialize plot controll
     popt.SetUseGerberAttributes(True)
     popt.SetExcludeEdgeLayer(False)
     popt.SetUseAuxOrigin(True)
-
-    # layers = [
-    #     ("F_Cu", pcbnew.F_Cu, "Top layer"),
-    #     ("B_Cu", pcbnew.B_Cu, "Bottom layer"),
-    #     ("B_Paste", pcbnew.B_Paste, "Paste bottom"),
-    #     ("F_Paste", pcbnew.F_Paste, "Paste top"),
-    #     ("F_SilkS", pcbnew.F_SilkS, "Silk top"),
-    #     ("B_SilkS", pcbnew.B_SilkS, "Silk top"),
-    #     ("B_Mask", pcbnew.B_Mask, "Mask bottom"),
-    #     ("F_Mask", pcbnew.F_Mask, "Mask top"),
-    #     ("Edge_Cuts", pcbnew.Edge_Cuts, "Edges"),
-    #     ("Margin", pcbnew.Margin, "Margin"),
-    #     ("In1_Cu", pcbnew.In1_Cu, "Inner1"),
-    #     ("In2_Cu", pcbnew.In2_Cu, "Inner2"),
-    #     ("Dwgs_User", pcbnew.Dwgs_User, "Dwgs_User"),
-    #     ("Cmts_User", pcbnew.Cmts_User, "Comments_User"),
-    #     ("Eco1_User", pcbnew.Eco1_User, "ECO1"),
-    #     ("Eco2_User", pcbnew.Eco2_User, "ECO2"),
-    #     ("B_Fab", pcbnew.B_Fab, "Fab bottom"),
-    #     ("F_Fab", pcbnew.F_Fab, "Fab top"),
-    #     ("B_Adhes", pcbnew.B_Adhes, "Adhesive bottom"),
-    #     ("F_Adhes", pcbnew.F_Adhes, "Adhesive top"),
-    #     ("B_CrtYd", pcbnew.B_CrtYd, "Courtyard bottom"),
-    #     ("F_CrtYd", pcbnew.F_CrtYd, "Courtyard top"),
-    # ]
 
     layers = [
         ("F_Cu",      pcbnew.F_Cu,      "Top copper"),
